483096ce304dbd1246747026a8b10ac94698af4e
d.bykov <d.bykov@NB-2973.local>
2023-04-22 19:27:13 +0300

    ADD: hook and change methodology

---
+++ src/app/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { useTelegram } from "../shared/hooks/use-telegram";
+
+function App() {
+  const { tg, user, onClose, onToggleButton } = useTelegram();
+
+  React.useEffect(() => {
+    tg.ready();
+  }, []);
+
+  console.log(user);
+
+  // add string on next line with tg user name from tg object
+
+  return (
+    <div className="App">
+      {user?.username}
+      <button onClick={onClose}>Close</button>
+      <button onClick={onToggleButton}>Toggle Button</button>
+    </div>
+  );
+}
+
+export default App;
